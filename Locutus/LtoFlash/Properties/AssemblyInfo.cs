44ef119e456e3040b379c468b1d2b17090f27afe
Steve Orth <Steve Orth>
2016-12-26 14:59:08 -0600

    LtoFlash - intermediate version info prior to 2375. Looks like that release had wrong sources published to web.

--- Locutus/LtoFlash/Properties/AssemblyInfo.cs
+++ Locutus/LtoFlash/Properties/AssemblyInfo.cs
@@ -70,4 +70,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.2350")]
-[assembly: AssemblyInformationalVersion("1.0.0 (Build 2350)")]
+[assembly: AssemblyInformationalVersion("1.0.0 (Build 2350 rrjz)")]

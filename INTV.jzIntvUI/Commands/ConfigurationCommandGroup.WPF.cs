e0cf55b93b240266541c7f91088370b85fea8df4
intvsteve <github@intvfunhouse.com>
2017-01-24 22:56:03 -0600

    Add Ribbon configuration options for jzIntv launch.

--- INTV.jzIntvUI/Commands/ConfigurationCommandGroup.WPF.cs
+++ INTV.jzIntvUI/Commands/ConfigurationCommandGroup.WPF.cs
@@ -18,6 +18,10 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 // </copyright>
 
+using INTV.Shared.Commands;
+using INTV.Shared.ComponentModel;
+using INTV.Shared.Utility;
+
 namespace INTV.JzIntvUI.Commands
 {
     /// <summary>
@@ -25,6 +29,117 @@ namespace INTV.JzIntvUI.Commands
     /// </summary>
     public partial class ConfigurationCommandGroup
     {
+        #region ConfigurationRibbonGroupCommand
+
+        /// <summary>
+        /// Command to act as grouper for jzIntv configuration commands.
+        /// </summary>
+        public static readonly VisualRelayCommand ConfigurationRibbonGroupCommand = new VisualRelayCommand(INTV.Shared.ComponentModel.RelayCommand.NoOp)
+        {
+            UniqueId = UniqueNameBase + ".ConfigurationRibbonGroupCommand",
+            Name = Resources.Strings.ConfigurationRibbonGroupCommand_Name,
+            LargeIcon = typeof(JzIntvLauncherCommandGroup).LoadImageResource("Resources/Images/settings_32xMD.png"),
+            SmallIcon = typeof(JzIntvLauncherCommandGroup).LoadImageResource("Resources/Images/settings_16xLG.png"),
+            VisualParent = JzIntvLauncherCommandGroup.JzIntvRibbonTabCommand,
+            Weight = 0.1,
+            UseXamlResource = true
+        };
+
+        #endregion // ConfigurationRibbonGroupCommand
+
+        #region SetEnableIntellivoiceCommand
+
+        /// <summary>
+        /// Command to configure jzIntv Intellivoice setting. (All the work is in the bindings to settings in the visual.)
+        /// </summary>
+        public static readonly VisualRelayCommand SetEnableIntellivoiceCommand = new VisualRelayCommand(INTV.Shared.ComponentModel.RelayCommand.NoOp)
+        {
+            UniqueId = UniqueNameBase + ".SetEnableIntellivoiceCommand",
+            Name = Resources.Strings.SettingsPage_General_EnableIntellivoice_Label,
+            ToolTip = Resources.Strings.SettingsPage_General_EnableIntellivoice_Tip,
+            ToolTipTitle = Resources.Strings.SettingsPage_General_EnableIntellivoice_Label,
+            SmallIcon = typeof(JzIntvLauncherCommandGroup).LoadImageResource("Resources/Images/intellivoice_16xLG.png"),
+            VisualParent = ConfigurationRibbonGroupCommand,
+            Weight = 0.0,
+            UseXamlResource = true
+        };
+
+        #endregion // SetEnableIntellivoiceCommand
+
+        #region SetEnableEcsCommand
+
+        /// <summary>
+        /// Command to configure jzIntv ECS setting. (All the work is in the bindings to settings in the visual.)
+        /// </summary>
+        public static readonly VisualRelayCommand SetEnableEcsCommand = new VisualRelayCommand(INTV.Shared.ComponentModel.RelayCommand.NoOp)
+        {
+            UniqueId = UniqueNameBase + ".SetEnableEcsCommand",
+            Name = Resources.Strings.SettingsPage_General_EnableECS_Label,
+            ToolTip = Resources.Strings.SettingsPage_General_EnableEcs_Tip,
+            ToolTipTitle = Resources.Strings.SettingsPage_General_EnableECS_Label,
+            SmallIcon = typeof(JzIntvLauncherCommandGroup).LoadImageResource("Resources/Images/ecs_16xLG.png"),
+            VisualParent = ConfigurationRibbonGroupCommand,
+            Weight = 0.1,
+            UseXamlResource = true
+        };
+
+        #endregion // SetEnableEcsCommand
+
+        #region SetEnableJlpCommand
+
+        /// <summary>
+        /// Command to configure jzIntv JLP setting. (All the work is in the bindings to settings in the visual.)
+        /// </summary>
+        public static readonly VisualRelayCommand SetEnableJlpCommand = new VisualRelayCommand(INTV.Shared.ComponentModel.RelayCommand.NoOp)
+        {
+            UniqueId = UniqueNameBase + ".SetEnableJlpCommand",
+            Name = Resources.Strings.SettingsPage_General_JLPFeatures_Label,
+            ToolTip = Resources.Strings.SettingsPage_General_EnableJlp_Tip,
+            ToolTipTitle = Resources.Strings.SettingsPage_General_JLPFeatures_Label,
+            SmallIcon = typeof(JzIntvLauncherCommandGroup).LoadImageResource("Resources/Images/jlp_9xSM.png"),
+            VisualParent = ConfigurationRibbonGroupCommand,
+            Weight = 0.2,
+            UseXamlResource = true
+        };
+
+        #endregion // SetEnableJlpCommand
+
+        #region MuteSoundCommand
+
+        /// <summary>
+        /// Command to configure jzIntv ECS setting. (All the work is in the bindings to settings in the visual.)
+        /// </summary>
+        public static readonly VisualRelayCommand MuteSoundCommand = new VisualRelayCommand(INTV.Shared.ComponentModel.RelayCommand.NoOp)
+        {
+            UniqueId = UniqueNameBase + ".MuteSoundCommand",
+            Name = Resources.Strings.SettingsPage_MuteSound_Label,
+            ToolTip = Resources.Strings.Configuration_MuteSound_Tip,
+            ToolTipTitle = Resources.Strings.SettingsPage_MuteSound_Label,
+            VisualParent = ConfigurationRibbonGroupCommand,
+            Weight = 0.3,
+            UseXamlResource = true
+        };
+
+        #endregion // MuteSoundCommand
+
+        #region ShowFullscreenCommand
+
+        /// <summary>
+        /// Command to configure jzIntv windowed / full-screen setting. (All the work is in the bindings to settings in the visual.)
+        /// </summary>
+        public static readonly VisualRelayCommand ShowFullscreenCommand = new VisualRelayCommand(INTV.Shared.ComponentModel.RelayCommand.NoOp)
+        {
+            UniqueId = UniqueNameBase + ".ShowFullscreenCommand",
+            Name = Resources.Strings.Configuration_EnableFullscreen,
+            ToolTip = Resources.Strings.Configuration_FullscreenMode_Tip,
+            ToolTipTitle = Resources.Strings.Configuration_EnableFullscreen,
+            VisualParent = ConfigurationRibbonGroupCommand,
+            Weight = 0.4,
+            UseXamlResource = true
+        };
+
+        #endregion // ShowFullscreenCommand
+
         private static string OSResolvePathFromSettings(string path)
         {
             return path;
@@ -34,5 +149,40 @@ namespace INTV.JzIntvUI.Commands
         {
             return path;
         }
+
+        private static void JzIntvSettingsChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            var status = GetConfigurationStatusString(false);
+            var configureGroupName = string.Format(Resources.Strings.ConfigurationRibbonGroupCommand_Name_Format, status);
+            ConfigurationRibbonGroupCommand.Name = configureGroupName;
+        }
+
+        #region CommandGroup
+
+        /// <summary>
+        /// WPF-specific command setup.
+        /// </summary>
+        partial void AddPlatformCommands()
+        {
+            OpenSettingsDialogCommand.VisualParent = ConfigurationRibbonGroupCommand;
+            OpenSettingsDialogCommand.Weight = 0.9;
+            OpenSettingsDialogCommand.UseXamlResource = true;
+
+            // Set up a handler to update group text with changes in configuration status.
+            Properties.Settings.Default.PropertyChanged += JzIntvSettingsChanged;
+            JzIntvSettingsChanged(null, null);
+
+            CommandList.Add(ConfigurationRibbonGroupCommand);
+            CommandList.Add(SetEnableIntellivoiceCommand);
+            CommandList.Add(SetEnableEcsCommand);
+            CommandList.Add(SetEnableJlpCommand);
+            CommandList.Add(MuteSoundCommand.CreateRibbonSeparator(CommandLocation.Before));
+            CommandList.Add(MuteSoundCommand);
+            CommandList.Add(ShowFullscreenCommand);
+            CommandList.Add(OpenSettingsDialogCommand.CreateRibbonSeparator(CommandLocation.Before));
+            CommandList.Add(OpenSettingsDialogCommand);
+        }
+
+        #endregion // CommandGroup
     }
 }
---
+++ INTV.jzIntvUI/Commands/JzIntvLauncherCommandGroup.WPF.cs
@@ -0,0 +1,148 @@
+﻿// <copyright file="JzIntvLauncherCommandGroup.WPF.cs" company="INTV Funhouse">
+// Copyright (c) 2016-2017 All Rights Reserved
+// <author>Steven A. Orth</author>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this software. If not, see: http://www.gnu.org/licenses/.
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+// </copyright>
+
+using INTV.Shared.Commands;
+using INTV.Shared.ComponentModel;
+using INTV.Shared.Utility;
+
+namespace INTV.JzIntvUI.Commands
+{
+    /// <summary>
+    /// WPF-specific commands.
+    /// </summary>
+    public partial class JzIntvLauncherCommandGroup
+    {
+        #region JzIntvRibbonTabCommand
+
+        /// <summary>
+        /// Pseudo-command for the jzIntv ribbon tab.
+        /// </summary>
+        public static readonly VisualRelayCommand JzIntvRibbonTabCommand = new VisualRelayCommand(INTV.Shared.ComponentModel.RelayCommand.NoOp)
+        {
+            UniqueId = UniqueNameBase + ".JzIntvRibbonTabCommand",
+            Name = Resources.Strings.JzIntv,
+            Weight = 0.09,
+            VisualParent = INTV.Shared.Commands.RootCommandGroup.RootCommand,
+            UseXamlResource = true
+        };
+
+        #endregion // JzIntvRibbonTabCommand
+
+        #region LaunchRibbonGroupCommand
+
+        /// <summary>
+        /// Command to act as grouper for various other, specific device commands.
+        /// </summary>
+        public static readonly VisualRelayCommand LaunchRibbonGroupCommand = new VisualRelayCommand(INTV.Shared.ComponentModel.RelayCommand.NoOp)
+        {
+            UniqueId = UniqueNameBase + ".LaunchRibbonGroupCommand",
+            Name = Resources.Strings.LaunchRibbonGroupCommand_Name,
+            LargeIcon = typeof(JzIntvLauncherCommandGroup).LoadImageResource("Resources/Images/jzIntvUI_32xMD.png"),
+            VisualParent = JzIntvRibbonTabCommand,
+            Weight = 0,
+            UseXamlResource = true
+        };
+
+        #endregion // LaunchRibbonGroupCommand
+
+        #region LaunchInJzIntvRibbonSplitButtonCommand
+
+        /// <summary>
+        /// Command for the Run in jzIntv ribbon split button in the jzIntv tab.
+        /// </summary>
+        public static readonly VisualRelayCommand LaunchInJzIntvRibbonSplitButtonCommand = new VisualRelayCommand(INTV.Shared.ComponentModel.RelayCommand.NoOp)
+        {
+            UniqueId = UniqueNameBase + ".LaunchInJzIntvRibbonSplitButtonCommand",
+            SmallIcon = typeof(JzIntvLauncherCommandGroup).LoadImageResource("Resources/Images/download_play_16xLG_color.png"),
+            LargeIcon = typeof(JzIntvLauncherCommandGroup).LoadImageResource("Resources/Images/download_play_32xLG_color.png"),
+            ToolTip = Resources.Strings.LaunchInJzIntvCommand_TipDescription,
+            ToolTipTitle = Resources.Strings.LaunchInJzIntvCommand_TipTitle,
+            ToolTipDescription = Resources.Strings.LaunchInJzIntvCommand_TipDescription,
+            ToolTipIcon = VisualRelayCommand.DefaultToolTipIcon,
+            Weight = 0,
+            VisualParent = LaunchRibbonGroupCommand,
+            UseXamlResource = true
+        };
+
+        #endregion // DownloadRibbonSplitButtonCommand
+
+        #region DownloadRibbonButtonCommand
+
+        /// <summary>
+        /// Command to load a ROM onto an Intellicart! to be inserted into the Play button in the Home tab.
+        /// </summary>
+        public static readonly VisualRelayCommand LaunchInJzIntvRibbonButtonCommand = new VisualRelayCommand(OnLaunch, CanLaunch)
+        {
+            UniqueId = UniqueNameBase + ".LaunchInJzIntvRibbonButtonCommand",
+            Name = Resources.Strings.LaunchInJzIntvCommand_MenuItemName,
+            MenuItemName = Resources.Strings.LaunchInJzIntvCommand_MenuItemName,
+            SmallIcon = typeof(JzIntvLauncherCommandGroup).LoadImageResource("Resources/Images/download_play_16xLG_color.png"),
+            LargeIcon = typeof(JzIntvLauncherCommandGroup).LoadImageResource("Resources/Images/download_play_32xLG_color.png"),
+            ToolTip = Resources.Strings.LaunchInJzIntvCommand_TipDescription,
+            ToolTipTitle = Resources.Strings.LaunchInJzIntvCommand_TipTitle,
+            ToolTipDescription = Resources.Strings.LaunchInJzIntvCommand_TipDescription + CommandProviderHelpers.RibbonSplitButtonExtraToolTipDescription,
+            ToolTipIcon = VisualRelayCommand.DefaultToolTipIcon,
+            Weight = 0.3,
+            VisualParent = RomListCommandGroup.RunProgramRibbonCommand,
+            UseXamlResource = true
+        };
+
+        #endregion // LaunchInJzIntvRibbonButtonCommand
+
+        #region BrowseAndLaunchInJzIntvRibbonButtonCommand
+
+        /// <summary>
+        /// The command to browse for a ROM, then load it for immediate execution on the Intellicart, inserted to Play button in the Home tab.
+        /// </summary>
+        public static readonly VisualRelayCommand BrowseAndLaunchInJzIntvRibbonButtonCommand = new VisualRelayCommand(BrowseAndDownload, CanBrowseAndDownload)
+        {
+            UniqueId = UniqueNameBase + ".BrowseAndLaunchInJzIntvRibbonButtonCommand",
+            Name = Resources.Strings.BrowseAndLaunchInJzIntvCommand_Name,
+            ToolTip = Resources.Strings.BrowseAndLaunchInJzIntvCommand_TipDescription,
+            ToolTipTitle = Resources.Strings.BrowseAndLaunchInJzIntvCommand_Name,
+            ToolTipDescription = Resources.Strings.BrowseAndLaunchInJzIntvCommand_TipDescription,
+            ToolTipIcon = VisualRelayCommand.DefaultToolTipIcon,
+            LargeIcon = typeof(JzIntvLauncherCommandGroup).LoadImageResource("Resources/Images/browse_download_play_32xLG.png"),
+            SmallIcon = typeof(JzIntvLauncherCommandGroup).LoadImageResource("Resources/Images/browse_download_play_16xLG.png"),
+            Weight = 0.31,
+            VisualParent = RomListCommandGroup.RunProgramRibbonCommand,
+            UseXamlResource = true
+        };
+
+        #endregion // BrowseAndLaunchInJzIntvRibbonButtonCommand
+
+        #region CommandGroup
+
+        /// <summary>
+        /// WPF-specific command setup.
+        /// </summary>
+        partial void AddPlatformCommands()
+        {
+            CommandList.Add(JzIntvRibbonTabCommand);
+            CommandList.Add(LaunchRibbonGroupCommand);
+            CommandList.Add(LaunchInJzIntvRibbonSplitButtonCommand);
+            CommandList.Add(LaunchInJzIntvRibbonButtonCommand.CreateRibbonMenuSeparator(CommandLocation.Before, false));
+            CommandList.Add(LaunchInJzIntvRibbonButtonCommand);
+            CommandList.Add(BrowseAndLaunchInJzIntvRibbonButtonCommand);
+        }
+
+        #endregion // CommandGroup
+    }
+}

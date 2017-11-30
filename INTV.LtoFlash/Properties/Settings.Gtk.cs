1b0956364c2ede3e4b6145d5e97d8261ba194ded
intvsteve <github@intvfunhouse.com>
2017-11-30 01:17:36 -0600

    Add settings implemenation for INTV.LtoFlash.

---
+++ INTV.LtoFlash/Properties/Settings.Gtk.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="Settings.Gtk.cs" company="INTV Funhouse">
+// Copyright (c) 2017 All Rights Reserved
+// <author>Steven A. Orth</author>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this software. If not, see: http://www.gnu.org/licenses/.
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+// </copyright>
+
+namespace INTV.LtoFlash.Properties
+{
+    internal sealed partial class Settings
+    {
+        /// <summary>
+        /// Gets or sets the width of the menu layout long name column.
+        /// </summary>
+        public int MenuLayoutLongNameColWidth
+        {
+            get { return GetSetting<int>(MenuLayoutLongNameColWidthSettingName); }
+            set { SetSetting(MenuLayoutLongNameColWidthSettingName, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the width of the menu layout short name column.
+        /// </summary>
+        public int MenuLayoutShortNameColWidth
+        {
+            get { return GetSetting<int>(MenuLayoutShortNameColWidthSettingName); }
+            set { SetSetting(MenuLayoutShortNameColWidthSettingName, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the width of the menu layout manual column.
+        /// </summary>
+        public int MenuLayoutManualColWidth
+        {
+            get { return GetSetting<int>(MenuLayoutManualColWidthSettingName); }
+            set { SetSetting(MenuLayoutManualColWidthSettingName, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the width of the menu layout save data column.
+        /// </summary>
+        public int MenuLayoutSaveDataColWidth
+        {
+            get { return GetSetting<int>(MenuLayoutSaveDataColWidthSettingName); }
+            set { SetSetting(MenuLayoutSaveDataColWidthSettingName, value); }
+        }
+
+        partial void OSInitializeDefaults()
+        {
+            AddSetting(MenuLayoutLongNameColWidthSettingName, 256);
+            AddSetting(MenuLayoutShortNameColWidthSettingName, 144);
+            AddSetting(MenuLayoutManualColWidthSettingName, 168);
+            AddSetting(MenuLayoutSaveDataColWidthSettingName, 128);
+        }
+    }
+}

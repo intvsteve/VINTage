b2861f693aa845d1cb154f5a7aa68cc77efe2f92
intvsteve <github@intvfunhouse.com>
2018-02-09 21:06:22 -0600

    Include a 'seed' version of Version.cs. Ideally, this would always be generated, but it requires access to the SVN repo, which is not public.

---
+++ VersionInfo.cs
@@ -0,0 +1,51 @@
+// <copyright file="VersionInfo.cs" company="INTV Funhouse">
+// Copyright (c) 2018 All Rights Reserved
+// <author>Steven A. Orth</author>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this software. If not, see: http://www.gnu.org/licenses/.
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+// </copyright>
+
+namespace INTV
+{
+    /// <summary>
+    /// This class contains the various version strings to use in a VINTage project.
+    /// It provides constants for use by assemblies in the various projects that
+    /// desire to use them.
+    /// </summary>
+    /// <remarks>THIS CLASS IS GENERATED! DO NOT MANUALLY EDIT THIS FILE!</remarks>
+    internal static class VersionInfo
+    {
+        /// <summary>
+        /// The full version string.
+        /// </summary>
+        public const string FullVersionString = "1.0.0.3753";
+
+        /// <summary>
+        /// The short version string.
+        /// </summary>
+        public const string ShortVersionString = "1.0.0";
+
+        /// <summary>
+        /// The build version string.
+        /// </summary>
+        public const string BuildVersionString = "Build 3753 (1 modified files)";
+
+        /// <summary>
+        /// The current copyright year string.
+        /// </summary>
+        public const string CurrentCopyrightYear = "2018";
+    }
+}

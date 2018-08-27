248237c0ad80ae8dd14e7a8e4e4d65426ba8a92a
Unknown <github@intvfunhouse.com>
2018-08-26 22:13:18 -0500

    Add core RomInformation files
    Added base files used to interact with the planned INTV Funhouse rominfo database. This code should be able to load an XML export of the database, based on phpMyAdmin and MySQL as installed on the site in August, 2018.

---
+++ INTV.Core/Model/Program/RomInformation.cs
@@ -0,0 +1,67 @@
+﻿// <copyright file="RomInformation.cs" company="INTV Funhouse">
+// Copyright (c) 2018 All Rights Reserved
+// <author>Steven A. Orth</author>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this software. If not, see: http://www.gnu.org/licenses/.
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+// </copyright>
+
+using System.Linq;
+
+namespace INTV.Core.Model.Program
+{
+    /// <summary>
+    /// This class represents an entry from the rominfo database at INTV Funhouse.
+    /// </summary>
+    [System.Xml.Serialization.XmlType("table")]
+    public class RomInformation
+    {
+        /// <summary>
+        /// Expected name of each "table" entry in the database. phpMyAdmin imports / exports the MySQL database as a
+        /// collection of tables, each named "rominfo" using a name attribute.
+        /// </summary>
+        public const string RomInfoDatabaseEntryName = "rominfo";
+
+        private const string RomInfoNameAttributeName = "name";
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="RomInformation"/>.
+        /// </summary>
+        public RomInformation()
+        {
+            RomInfoDatabaseColumns = Enumerable.Empty<RomInformationDatabaseColumn>().ToArray();
+        }
+
+        #region Properties
+
+        #region XML Properties
+
+        /// <summary>
+        /// Gets or sets the name of the database entry. This is presumed to be the value of <see cref="RomInfoDatabaseEntryName"/>.
+        /// </summary>
+        [System.Xml.Serialization.XmlAttribute(RomInfoNameAttributeName)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ROM info database columns array.
+        /// </summary>
+        [System.Xml.Serialization.XmlElement(RomInformationDatabaseColumn.RomInfoColumnXmlTypeName, typeof(RomInformationDatabaseColumn))]
+        public RomInformationDatabaseColumn[] RomInfoDatabaseColumns { get; set; }
+
+        #endregion // XML Properties
+
+        #endregion // Properties
+    }
+}
---
+++ INTV.Core/Model/Program/RomInformationDatabase.cs
@@ -0,0 +1,55 @@
+﻿// <copyright file="RomInformationDatabase.cs" company="INTV Funhouse">
+// Copyright (c) 2018 All Rights Reserved
+// <author>Steven A. Orth</author>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this software. If not, see: http://www.gnu.org/licenses/.
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+// </copyright>
+
+using System.Linq;
+
+namespace INTV.Core.Model.Program
+{
+    /// <summary>
+    /// This class represents the rominfo database as exported from phpMyAdmin at INTV Funhouse.
+    /// </summary>
+    /// <remarks>NOTE: This may be fragile, depending on the version of MySQL and phpMyAdmin used at the website.</remarks>
+    [System.Xml.Serialization.XmlRoot(DatabaseRootName)]
+    public class RomInformationDatabase
+    {
+        private const string DatabaseRootName = "pma_xml_export";
+        private const string DatabaseName = "database";
+        private const string RomInfoEntryXmlName = "table";
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="RomInformationDatabase"/>.
+        /// </summary>
+        public RomInformationDatabase()
+        {
+            RomInfos = Enumerable.Empty<RomInformation>().ToArray();
+        }
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the collection of <see cref="RomInformation"/> objects in the database.
+        /// </summary>
+        [System.Xml.Serialization.XmlArray(DatabaseName)]
+        [System.Xml.Serialization.XmlArrayItem(RomInfoEntryXmlName, typeof(RomInformation))]
+        public RomInformation[] RomInfos { get; set; }
+
+        #endregion // Properties
+    }
+}

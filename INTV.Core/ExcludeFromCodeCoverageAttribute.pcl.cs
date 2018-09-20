6937a0954db8739509a48eaf19e81b7be2154b4a
Unknown <github@intvfunhouse.com>
2018-09-19 23:18:39 -0500

    Added dummy ExcludeFromCodeCoverageAttribute
    This is for Portable Class Library use only. Merely having an implementation of this attribute in the expected namespace suffices to disable code coverage. Conditional, private methods (e.g. various internal debugging facilities that are compiled out) shouldn't confuse the code coverage tools.
    
    Sadly, anonymous methods (lambdas) that are in conditional methods that are to be ignored still show up as missing in code coverage. :P

---
+++ INTV.Core/ExcludeFromCodeCoverageAttribute.pcl.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="ExcludeFromCodeCoverageAttribute.pcl.cs" company="INTV Funhouse">
+// Copyright (c) 2018 All Rights Reserved
+// <author>Steven A. Orth</author>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this software. If not, see: http://www.gnu.org/licenses/.
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+// </copyright>
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    /// <summary>
+    /// Specifies that the attributed code should be excluded from code coverage information.
+    /// </summary>
+    /// <remarks>
+    /// This attribute was added to the assembly because it's not otherwise available
+    /// to portable class libraries.
+    /// See: https://stackoverflow.com/questions/31776815/excludefromcodecoverage-attribute-is-not-supported-within-the-portable-class-lib
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Event, AllowMultiple = false, Inherited = false)]
+    internal sealed class ExcludeFromCodeCoverageAttribute : Attribute
+    {
+    }
+}

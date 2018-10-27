ee2a1fa8cb1d87a824a1fb147fd9244a9afbbcbe
Unknown <github@intvfunhouse.com>
2018-10-27 11:34:10 -0500

    Fix another WeakKeyDictionary test
    Entries in the dictionary were getting garbage collected during the test. Oops.

--- Tests/INTV.Core.Tests/Utility/WeakKeyDictionaryTests.cs
+++ Tests/INTV.Core.Tests/Utility/WeakKeyDictionaryTests.cs
@@ -475,10 +475,13 @@ namespace INTV.Core.Tests.Utility
         public void WeakKeyDictionaryWithData_CopyToNullKeyValuePairArray_ThrowsArgumentNullException()
         {
             const int NumValuesToAdd = 10;
+            var keeperArounder = new List<KeyValuePair<string, int>>();
             ICollection<KeyValuePair<string, int>> dictionary = new WeakKeyDictionary<string, int>();
             for (int i = 0; i < NumValuesToAdd; ++i)
             {
-                dictionary.Add(new KeyValuePair<string, int>(i.ToString(), i));
+                var entry = new KeyValuePair<string, int>(i.ToString(), i);
+                keeperArounder.Add(entry);
+                dictionary.Add(entry);
             }
             Assert.Equal(NumValuesToAdd, dictionary.Count);
 

e659732fb90a06af82b3c352e85c192747dc56e8
Unknown <github@intvfunhouse.com>
2018-11-22 10:43:33 -0600

    Add AddColumn and RemoveColumn to XmlRomInforfmation
    Helps with testing, and could potentially be somewhat useful.

--- Tests/INTV.Core.Tests/Model/Program/XmlRomInformationTests.cs
+++ Tests/INTV.Core.Tests/Model/Program/XmlRomInformationTests.cs
@@ -67,5 +67,72 @@ namespace INTV.Core.Tests.Model.Program
                 Assert.NotNull(xmlRomInformation.GetColumn(columnName, requiredColumn: true));
             }
         }
+
+        [Fact]
+        public void XmlRomInformation_AddColumnToEmptyInstance_AddsColumn()
+        {
+            var xmlRomInformation = new XmlRomInformation();
+            Assert.Equal(0, xmlRomInformation.RomInfoDatabaseColumns.Length);
+
+            xmlRomInformation.AddColumn(XmlRomInformationDatabaseColumnName.format, null);
+
+            Assert.NotNull(xmlRomInformation.GetColumn(XmlRomInformationDatabaseColumnName.format));
+        }
+
+        [Fact]
+        public void XmlRomInformation_AddColumnToInstanceWithNullColumns_AddsColumn()
+        {
+            var xmlRomInformation = new XmlRomInformation();
+            xmlRomInformation.RomInfoDatabaseColumns = null;
+            Assert.Null(xmlRomInformation.RomInfoDatabaseColumns);
+
+            xmlRomInformation.AddColumn(XmlRomInformationDatabaseColumnName.format, null);
+
+            Assert.NotNull(xmlRomInformation.GetColumn(XmlRomInformationDatabaseColumnName.format));
+        }
+
+        [Fact]
+        public void XmlRomInformation_AddColumnToDefaultInstance_ThrowsArgumentException()
+        {
+            var xmlRomInformation = XmlRomInformation.CreateDefault();
+
+            Assert.Throws<ArgumentException>(() => xmlRomInformation.AddColumn(XmlRomInformationDatabaseColumnName.format, null));
+        }
+
+        [Fact]
+        public void XmlRomInformation_RemoveColumnFromEmptyInstance_ReturnsFalse()
+        {
+            var xmlRomInformation = new XmlRomInformation();
+            Assert.Equal(0, xmlRomInformation.RomInfoDatabaseColumns.Length);
+
+            Assert.False(xmlRomInformation.RemoveColumn(XmlRomInformationDatabaseColumnName.format));
+        }
+
+        [Fact]
+        public void XmlRomInformation_RemoveColumnFromInstanceWithNullColumns_ReturnsFalse()
+        {
+            var xmlRomInformation = new XmlRomInformation();
+            xmlRomInformation.RomInfoDatabaseColumns = null;
+            Assert.Null(xmlRomInformation.RomInfoDatabaseColumns);
+
+            Assert.False(xmlRomInformation.RemoveColumn(XmlRomInformationDatabaseColumnName.format));
+        }
+
+        [Fact]
+        public void XmlRomInformation_RemoveColumnFromDefaultInstance_ReturnsTrue()
+        {
+            var xmlRomInformation = XmlRomInformation.CreateDefault();
+
+            Assert.True(xmlRomInformation.RemoveColumn(XmlRomInformationDatabaseColumnName.game_docs));
+        }
+
+        [Fact]
+        public void XmlRomInformation_RemoveColumnTwiceFromDefaultInstance_ReturnsFalse()
+        {
+            var xmlRomInformation = XmlRomInformation.CreateDefault();
+
+            Assert.True(xmlRomInformation.RemoveColumn(XmlRomInformationDatabaseColumnName.game_docs));
+            Assert.False(xmlRomInformation.RemoveColumn(XmlRomInformationDatabaseColumnName.game_docs));
+        }
     }
 }

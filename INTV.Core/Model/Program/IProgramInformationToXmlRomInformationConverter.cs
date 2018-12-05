ee47f17ad048a4114149cad32384197d34a74145
Unknown <github@intvfunhouse.com>
2018-12-05 01:02:59 -0600

    Make IProgramInformationToXmlRomInformationConverter null tolerant
    Should be consistent about tolerating null Features and Year.
    
    Also: renamed string constants (fix build).

--- INTV.Core/Model/Program/IProgramInformationToXmlRomInformationConverter.cs
+++ INTV.Core/Model/Program/IProgramInformationToXmlRomInformationConverter.cs
@@ -80,13 +80,20 @@ namespace INTV.Core.Model.Program
             xmlRomInformation.GetColumn(XmlRomInformationDatabaseColumnName.vendor).Value = source.Vendor.EncodeHtmlString();
             xmlRomInformation.GetColumn(XmlRomInformationDatabaseColumnName.short_name).Value = source.ShortName.EncodeHtmlString();
             xmlRomInformation.GetColumn(XmlRomInformationDatabaseColumnName.origin).Value = ProgramInformationOriginToDatabaseString.Instance.Convert(source.DataOrigin);
-            xmlRomInformation.GetColumn(XmlRomInformationDatabaseColumnName.type).Value = GeneralFeaturesToRomTypeStringConverter.Instance.Convert(source.Features.GeneralFeatures);
 
-            int year;
-            if ((source.Year.Length == 4) && int.TryParse(source.Year, NumberStyles.None, CultureInfo.InvariantCulture, out year))
+            if (source.Features != null)
             {
-                // Only have year, so assume 01 Jan. This will be superseded if metadata contains release date (taking first one).
-                xmlRomInformation.GetColumn(XmlRomInformationDatabaseColumnName.release_date).Value = string.Format(CultureInfo.InvariantCulture, "{0:0000}-01-01", year);
+                xmlRomInformation.GetColumn(XmlRomInformationDatabaseColumnName.type).Value = GeneralFeaturesToRomTypeStringConverter.Instance.Convert(source.Features.GeneralFeatures);
+            }
+
+            if (!string.IsNullOrEmpty(source.Year))
+            {
+                int year;
+                if ((source.Year.Length == 4) && int.TryParse(source.Year, NumberStyles.None, CultureInfo.InvariantCulture, out year))
+                {
+                    // Only have year, so assume 01 Jan. This will be superseded if metadata contains release date (taking first one).
+                    xmlRomInformation.GetColumn(XmlRomInformationDatabaseColumnName.release_date).Value = string.Format(CultureInfo.InvariantCulture, "{0:0000}-01-01", year);
+                }
             }
 
             if (!string.IsNullOrEmpty(romVariant.Description))
@@ -106,34 +113,34 @@ namespace INTV.Core.Model.Program
                 {
                     case ProgramInformationOrigin.IntvFunhouse:
                     case ProgramInformationOrigin.Embedded:
-                        originString = XmlRomInformationDatabaseColumn.SourceIntvFunhouse;
+                        originString = XmlRomInformationDatabaseColumn.OriginIntvFunhouse;
                         break;
                     case ProgramInformationOrigin.IntellivisionProductions:
-                        originString = XmlRomInformationDatabaseColumn.SourceBlueSkyRangers;
+                        originString = XmlRomInformationDatabaseColumn.OriginBlueSkyRangers;
                         break;
                     case ProgramInformationOrigin.UserDefined:
-                        originString = XmlRomInformationDatabaseColumn.SourceUserDefined;
+                        originString = XmlRomInformationDatabaseColumn.OriginUserDefined;
                         break;
                     case ProgramInformationOrigin.UserEmail:
-                        originString = XmlRomInformationDatabaseColumn.SourceUserEmail;
+                        originString = XmlRomInformationDatabaseColumn.OriginUserEmail;
                         break;
                     case ProgramInformationOrigin.JzIntv:
-                        originString = XmlRomInformationDatabaseColumn.SourceIntvName;
+                        originString = XmlRomInformationDatabaseColumn.OriginIntvName;
                         break;
                     case ProgramInformationOrigin.RomMetadataBlock:
-                        originString = XmlRomInformationDatabaseColumn.SourceRomFormatMetadata;
+                        originString = XmlRomInformationDatabaseColumn.OriginRomFormatMetadata;
                         break;
                     case ProgramInformationOrigin.CfgVarMetadataBlock:
-                        originString = XmlRomInformationDatabaseColumn.SourceCfgFormatMetadata;
+                        originString = XmlRomInformationDatabaseColumn.OriginCfgFormatMetadata;
                         break;
                     case ProgramInformationOrigin.LuigiMetadataBlock:
-                        originString = XmlRomInformationDatabaseColumn.SourceLuigiFormatMetadata;
+                        originString = XmlRomInformationDatabaseColumn.OriginLuigiFormatMetadata;
                         break;
                     case ProgramInformationOrigin.GameCatalog:
-                        originString = XmlRomInformationDatabaseColumn.SourceCatalog;
+                        originString = XmlRomInformationDatabaseColumn.OriginCatalog;
                         break;
                     default:
-                        originString = XmlRomInformationDatabaseColumn.SourceOther;
+                        originString = XmlRomInformationDatabaseColumn.OriginOther;
                         break;
                 }
                 return originString;

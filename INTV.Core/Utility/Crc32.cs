e7d5853009f9cd2cd793fb529ef7d0b7966b683e
Unknown <github@intvfunhouse.com>
2019-02-03 13:31:38 -0600

    Add tests for Castagnoli polynomial
    Tests and initial finish, prep work for more completely validating LUIGI data blocks.

--- INTV.Core/Utility/Crc32.cs
+++ INTV.Core/Utility/Crc32.cs
@@ -1,4 +1,4 @@
-// <copyright file="Crc32.cs" company="INTV Funhouse">
+﻿// <copyright file="Crc32.cs" company="INTV Funhouse">
 // Copyright (c) 2014-2019 All Rights Reserved
 // <author>Steven A. Orth</author>
 //

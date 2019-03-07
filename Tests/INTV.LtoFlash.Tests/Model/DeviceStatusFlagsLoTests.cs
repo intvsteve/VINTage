b4fb3a8ce0d687ccd205fc205ec44bc2bdf895e2
Unknown <github@intvfunhouse.com>
2019-03-06 23:41:50 -0600

    Introduce unit tests for INTV.LtoFlash
    Going to be taking it easy here, but as this work is introducing a new flag to device status, *and* intending to correct behavior when "reserved" flags may be put into use by firmware in an out-of-LUI-band update, the intent of the tests is to ensure the old LUI doesn't interfere with the intentions of the new firmware's flag usage.

---
+++ Tests/INTV.LtoFlash.Tests/Model/DeviceStatusFlagsLoTests.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="DeviceStatusFlagsLoTests.cs" company="INTV Funhouse">
+// Copyright (c) 2019 All Rights Reserved
+// <author>Steven A. Orth</author>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this software. If not, see: http://www.gnu.org/licenses/.
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using INTV.LtoFlash.Model;
+using Xunit;
+
+namespace INTV.LtoFlash.Tests.Model
+{
+    public class DeviceStatusFlagsLoTests
+    {
+        public static IEnumerable<object[]> DeviceStatusFlagsLoToHardwareStatusFlagsTestData
+        {
+            get
+            {
+                yield return new object[] { (object)DeviceStatusFlagsLo.None, (object)HardwareStatusFlags.None };
+                for (var i = 0; i < sizeof(DeviceStatusFlagsLo) * 8; ++i)
+                {
+                    var deviceStatusFlagsLo = (DeviceStatusFlagsLo)(1ul << i);
+                    var expectedHardwareFlags = HardwareStatusFlags.None;
+                    if (i < sizeof(HardwareStatusFlags) * 8)
+                    {
+                        expectedHardwareFlags = (HardwareStatusFlags)(1u << i);
+                    }
+                    yield return new object[] { (object)deviceStatusFlagsLo, (object)expectedHardwareFlags };
+                }
+            }
+        }
+
+        [Theory]
+        [MemberData("DeviceStatusFlagsLoToHardwareStatusFlagsTestData")]
+        public void DeviceStatusFlagsLo_ToHardwareStatusFlags_ReturnsExpectedFlags(DeviceStatusFlagsLo statusFlagsLo, HardwareStatusFlags expectedHardwareStatusFlags)
+        {
+            var actualHardwareStatusFlags = statusFlagsLo.ToHardwareStatusFlags();
+
+            Assert.Equal(expectedHardwareStatusFlags, actualHardwareStatusFlags);
+        }
+    }
+}

2e1185b721540e8b636b662c675397e686f99419
intvsteve <github@intvfunhouse.com>
2019-05-11 23:03:17 -0500

    Make RelayCommandBase on Mac subclass of NSObject
    This will allows binding (via KeyValuePair / NSObject.Bind) - which lends itself
    well to binding generally.

---
+++ INTV.Core/ComponentModel/RelayCommandBase.Mac.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="RelayCommandBase.Mac.cs" company="INTV Funhouse">
+// Copyright (c) 2019 All Rights Reserved
+// <author>Steven A. Orth</author>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this software. If not, see: http://www.gnu.org/licenses/.
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+// </copyright>
+
+#if __UNIFIED__
+using CommandBase = Foundation.NSObject;
+#else
+using CommandBase = MonoMac.Foundation.NSObject;
+#endif
+
+namespace INTV.Core.ComponentModel
+{
+    /// <summary>
+    /// Mac-specific implementation.
+    /// </summary>
+    public partial class RelayCommandBase : CommandBase
+    {
+    }
+}

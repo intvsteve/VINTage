5a233d8ead67e4a32f54e1ea51cbc2e4304e8c4d
Unknown <github@intvfunhouse.com>
2019-05-22 00:24:52 -0500

    Tests for new read-only configurable feature

--- Tests/INTV.Core.Tests/Model/Device/ConfigurableFeatureTests.cs
+++ Tests/INTV.Core.Tests/Model/Device/ConfigurableFeatureTests.cs
@@ -37,38 +37,38 @@ namespace INTV.Core.Tests.Model.Device
         [InlineData("", "Kinda silly")]
         public void ConfigurableFeature_CreateWithInvalidArgument_ThrowsArgumentOutOfRangeException(string uniqueId, string displayName)
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => new ConfigurableTestFeature(uniqueId, displayName));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ConfigurableIntTestFeature(uniqueId, displayName));
         }
 
         [Fact]
         public void ConfigurableFeature_GetFactoryDefaultValueAfterCreating_ReturnsFactoryDefaultValue()
         {
-            var feature = new ConfigurableTestFeature("GetFactoryDefaultTest");
+            var feature = new ConfigurableIntTestFeature("GetFactoryDefaultTest");
 
-            Assert.Equal(ConfigurableTestFeature.FactoryDefault, feature.FactoryDefaultValue);
-            Assert.Equal(ConfigurableTestFeature.FactoryDefault, ((IConfigurableFeature)feature).FactoryDefaultValue);
+            Assert.Equal(ConfigurableIntTestFeature.FactoryDefault, feature.FactoryDefaultValue);
+            Assert.Equal(ConfigurableIntTestFeature.FactoryDefault, ((IConfigurableFeature)feature).FactoryDefaultValue);
         }
 
         [Fact]
         public void ConfigurableFeature_GetCurrentValueAfterCreating_ReturnsDefaultValue()
         {
-            var feature = new ConfigurableTestFeature("GetCurrentValueAfterCreateTest");
+            var feature = new ConfigurableIntTestFeature("GetCurrentValueAfterCreateTest");
 
-            Assert.Equal(ConfigurableTestFeature.FactoryDefault, feature.CurrentValue);
+            Assert.Equal(ConfigurableIntTestFeature.FactoryDefault, feature.CurrentValue);
         }
 
         [Fact]
         public void ConfigurableFeature_GetValueFromDevice_ReturnsDefaultValueIfNeverWrittenTo()
         {
-            var feature = new ConfigurableTestFeature("GetValueFromDeviceAfterCreateTest");
+            var feature = new ConfigurableIntTestFeature("GetValueFromDeviceAfterCreateTest");
 
-            Assert.Equal(ConfigurableTestFeature.FactoryDefault, ((IConfigurableFeature)feature).GetValueFromDevice(null));
+            Assert.Equal(ConfigurableIntTestFeature.FactoryDefault, ((IConfigurableFeature)feature).GetValueFromDevice(null));
         }
 
         [Fact]
         public void ConfigurableFeature_SetValueOnDevice_ChangesCurrentValue()
         {
-            var feature = new ConfigurableTestFeature("SetValueOnDeviceTest");
+            var feature = new ConfigurableIntTestFeature("SetValueOnDeviceTest");
 
             var newValue = 0x12345;
             ((IConfigurableFeature)feature).SetValueOnDevice(null, newValue);
@@ -77,16 +77,89 @@ namespace INTV.Core.Tests.Model.Device
             Assert.Equal(newValue, ((IConfigurableFeature)feature).CurrentValue);
         }
 
-        private class ConfigurableTestFeature : ConfigurableFeature<int>
+        [Fact]
+        public void ConfigurableFeature_SetObjectValueOnDevice_ChangesCurrentValue()
+        {
+            var feature = new ConfigurableTestFeature("SetObjectValueOnDeviceTest");
+
+            var newValue = new object();
+            feature.SetValueOnDevice(null, newValue);
+
+            Assert.Equal(newValue, feature.CurrentValue);
+        }
+
+        [Fact]
+        public void ReadOnlyConfigurableFeature_SetValueOnDevice_ThrowsInvalidOperationException()
+        {
+            var feature = new ReadOnlyConfigurableTestFeature("ReadOnlyConfigurableFeature");
+
+            Assert.Throws<InvalidOperationException>(() => feature.SetValueOnDevice(null, 0x54321));
+        }
+
+        [Fact]
+        public void GenericIntReadOnlyConfigurableIntFeature_SetValueOnDevice_ThrowsInvalidOperationException()
+        {
+            IConfigurableFeature feature = new ReadOnlyConfigurableIntTestFeature();
+
+            Assert.Throws<InvalidOperationException>(() => feature.SetValueOnDevice(null, 0x54321));
+        }
+
+        [Fact]
+        public void ReadOnlyConfigurableGenericIntFeature_SetValueOnDevice_ThrowsInvalidOperationException()
+        {
+            IConfigurableFeature<int> feature = new ReadOnlyConfigurableGenericIntTestFeature();
+
+            Assert.Throws<InvalidOperationException>(() => feature.SetValueOnDevice(null, 0x54321));
+        }
+
+        private class ConfigurableTestFeature : IConfigurableFeature
+        {
+            public ConfigurableTestFeature(string name)
+            {
+                DisplayName = name;
+                UniqueId = name;
+                FactoryDefaultValue = name;
+                CurrentValue = name;
+            }
+
+            public string DisplayName { get; private set; }
+
+            public string UniqueId { get; private set; }
+
+            public object FactoryDefaultValue { get; private set; }
+
+            public object CurrentValue { get; private set; }
+
+            public object GetValueFromDevice(IPeripheral device)
+            {
+                return CurrentValue;
+            }
+
+            public void SetValueOnDevice(IPeripheral device, object newValue)
+            {
+                this.VerifyWriteAccess();
+                CurrentValue = newValue;
+            }
+        }
+
+        private class ReadOnlyConfigurableTestFeature : ConfigurableTestFeature, IReadOnlyConfigurableFeature
+        {
+            public ReadOnlyConfigurableTestFeature(string name)
+                : base("ReadOnly" + name)
+            {
+            }
+        }
+
+        private class ConfigurableIntTestFeature : ConfigurableFeature<int>
         {
             public const int FactoryDefault = -1;
 
-            public ConfigurableTestFeature(string name)
+            public ConfigurableIntTestFeature(string name)
                 : this(name, name)
             {
             }
 
-            public ConfigurableTestFeature(string uniqueId, string displayName)
+            public ConfigurableIntTestFeature(string uniqueId, string displayName)
                 : base(uniqueId, displayName, FactoryDefault)
             {
             }
@@ -99,6 +172,37 @@ namespace INTV.Core.Tests.Model.Device
             public override void SetValueOnDevice(IPeripheral device, int newValue)
             {
                 CurrentValue = newValue;
+            }
+        }
+
+        private class ReadOnlyConfigurableGenericIntTestFeature : ConfigurableIntTestFeature, IReadOnlyConfigurableFeature<int>
+        {
+            public ReadOnlyConfigurableGenericIntTestFeature()
+                : base("ReadOnly<int>")
+            {
+            }
+        }
+
+        private class ReadOnlyConfigurableIntTestFeature : ConfigurableIntTestFeature, IReadOnlyConfigurableFeature
+        {
+            public ReadOnlyConfigurableIntTestFeature()
+                : base("ReadOnly")
+            {
+            }
+
+            object IReadOnlyConfigurableFeature.FactoryDefaultValue
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            object IReadOnlyConfigurableFeature.CurrentValue
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            object IReadOnlyConfigurableFeature.GetValueFromDevice(IPeripheral device)
+            {
+                throw new NotImplementedException();
             }
         }
     }

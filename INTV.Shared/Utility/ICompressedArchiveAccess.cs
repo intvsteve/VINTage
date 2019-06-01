745e55c2527bf018bea0765ba5f00eab91644137
Unknown <github@intvfunhouse.com>
2019-06-01 14:52:38 -0500

    Add interfaces for accessing (compressed) archives

---
+++ INTV.Shared/Utility/ICompressedArchiveAccess.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="ICompressedArchiveAccess.cs" company="INTV Funhouse">
+// Copyright (c) 2019 All Rights Reserved
+// <author>Steven A. Orth</author>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this software. If not, see: http://www.gnu.org/licenses/.
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace INTV.Shared.Utility
+{
+    /// <summary>
+    /// A basic interface that provides access to an archive, which may or may not be compressed.
+    /// </summary>
+    public interface ICompressedArchiveAccess : IDisposable
+    {
+        /// <summary>
+        /// Gets a value indicating whether the archive is also compressed.
+        /// </summary>
+        bool IsCompressed { get; }
+
+        /// <summary>
+        /// Gets an enumerable of the entries contained in the archive.
+        /// </summary>
+        IEnumerable<ICompressedArchiveEntry> Entries { get; }
+
+        /// <summary>
+        /// Opens a stream that provides access to the contents of the entry.
+        /// </summary>
+        /// <param name="entry">The entry to get a stream for.</param>
+        /// <returns>A stream that may be used to access the contents of the entry.</returns>
+        /// <remarks>Whether the stream provides read or write access (or both) is implementation-defined.</remarks>
+        Stream OpenEntry(ICompressedArchiveEntry entry);
+
+        /// <summary>
+        /// Creates a new entry in the archive with the given name. Use <see cref="Open(ICompressedArchiveEntry)"/> to provide content.
+        /// </summary>
+        /// <param name="name">The name of the new entry in the archive.</param>
+        /// <returns>The new entry.</returns>
+        /// <remarks>Whether a new entry can be created depends on the implementation.</remarks>
+        ICompressedArchiveEntry CreateEntry(string name);
+    }
+}
---
+++ INTV.Shared/Utility/ICompressedArchiveEntry.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="ICompressedArchiveEntry.cs" company="INTV Funhouse">
+// Copyright (c) 2019 All Rights Reserved
+// <author>Steven A. Orth</author>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this software. If not, see: http://www.gnu.org/licenses/.
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+// </copyright>
+
+using System;
+
+namespace INTV.Shared.Utility
+{
+    /// <summary>
+    /// This interface describes a basic archive entry.
+    /// </summary>
+    public interface ICompressedArchiveEntry
+    {
+        /// <summary>
+        /// Gets the name of the entry within the archive.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Gets the size of the uncompressed archive entry.
+        /// </summary>
+        long Length { get; }
+
+        /// <summary>
+        /// Gets the last modification date and time of the archive entry.
+        /// </summary>
+        DateTime LastModificationType { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether or not the entry describes a directory.
+        /// </summary>
+        bool IsDirectory { get; }
+    }
+}

662b1147f41a5e0f10f7e7a603da76b9ac646f18
intvsteve <github@intvfunhouse.com>
2019-09-05 08:28:15 -0500

    Add IApplicationInfo implementation to LtoFlash
    Another step for #325.

---
+++ Locutus/LtoFlash/LtoFlashApplicationInfo.Win.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="LtoFlashApplicationInfo.Win.cs" company="INTV Funhouse">
+// Copyright (c) 2019 All Rights Reserved
+// <author>Steven A. Orth</author>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this software. If not, see: http://www.gnu.org/licenses/.
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+// </copyright>
+
+using INTV.Shared.Utility;
+
+namespace Locutus
+{
+    /// <summary>
+    /// Windows-specific implementation.
+    /// </summary>
+    internal sealed partial class ApplicationInfo
+    {
+        private const string OSString = "win";
+        private static readonly OSVersion OSVersionMinimum = new OSVersion(5, 1, 0); // Windows xp
+        private static readonly OSVersion OSVersionRecommended = new OSVersion(6, 1, 0); // Windows 7 or later
+    }
+}
---
+++ Locutus/LtoFlash/LtoFlashApplicationInfo.cs
@@ -0,0 +1,97 @@
+﻿// <copyright file="LtoFlashApplicationInfo.cs" company="INTV Funhouse">
+// Copyright (c) 2019 All Rights Reserved
+// <author>Steven A. Orth</author>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this software. If not, see: http://www.gnu.org/licenses/.
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+// </copyright>
+
+using System;
+using INTV.Shared.ComponentModel;
+using INTV.Shared.Properties;
+using INTV.Shared.Utility;
+
+namespace Locutus
+{
+    /// <summary>
+    /// Implements <see cref="IApplicationInfo"/> for the program.
+    /// </summary>
+    internal sealed partial class LtoFlashApplicationInfo : ApplicationInfo
+    {
+        private const string BaseProductUrl = "https://www.intvfunhouse.com/intvfunhouse/ltoflash/";
+        private readonly Lazy<string> _helpUrl = new Lazy<string>(InitializeHelpUrl);
+
+        /// <inheritdoc />
+        public override string Name
+        {
+            get { return "LTO Flash"; }
+        }
+
+        /// <inheritdoc />
+        public override string DisplayName
+        {
+            get { return Resources.Strings.ApplicationDisplayName; }
+        }
+
+        /// <inheritdoc />
+        public override string DocumentFolderName
+        {
+            get { return "LTO Flash"; }
+        }
+
+        /// <inheritdoc />
+        public override OSVersion MinimumOSVersion
+        {
+            get { return OSVersionMinimum; }
+        }
+
+        /// <inheritdoc />
+        public override OSVersion RecommendedOSVersion
+        {
+            get { return OSVersionRecommended; }
+        }
+
+        /// <inheritdoc />
+        public override string ProductUrl
+        {
+            get { return BaseProductUrl; }
+        }
+
+        /// <inheritdoc />
+        public override string OnlineHelpUrl
+        {
+            get { return _helpUrl.Value; }
+        }
+
+        /// <inheritdoc />
+        public override string VersionCheckUrl
+        {
+            get { return ProductUrl + "current_version.php?os=" + OSString; }
+        }
+
+        /// <inheritdoc />
+        public override ISettings Settings
+        {
+            get { return LtoFlash.Properties.Settings.Default; }
+        }
+
+        private static string InitializeHelpUrl()
+        {
+            var versionParts = StandardVersion.Split('.');
+            var helpPostData = "os=" + OSString + "&ver=" + versionParts[0] + versionParts[1];
+            return BaseProductUrl + "help/?" + helpPostData;
+        }
+    }
+}

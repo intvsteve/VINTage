08eba846b12b5a7901aef4c8e5820cb26ecb5928
Unknown <github@intvfunhouse.com>
2019-10-01 23:33:01 -0500

    Add compressed archive settings page for Windows

---
+++ INTV.Shared/View/CompressedArchiveAccessSettings.xaml.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="CompressedArchiveAccessSettings.xaml.cs" company="INTV Funhouse">
+// Copyright (c) 2019 All Rights Reserved
+// <author>Steven A. Orth</author>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 2 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this software. If not, see: http://www.gnu.org/licenses/.
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+// </copyright>
+
+using System.Windows.Controls;
+
+namespace INTV.Shared.View
+{
+    /// <summary>
+    /// Interaction logic for CompressedArchiveAccessSettings.xaml
+    /// </summary>
+    public partial class CompressedArchiveAccessSettings : UserControl
+    {
+        public CompressedArchiveAccessSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
